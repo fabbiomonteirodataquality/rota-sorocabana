20496d4dce3e8fe7db7222d40f90676f735b8c5b
fabbiomonteirodataquality <50607094+fabbiomonteirodataquality@users.noreply.github.com>
2025-04-20 12:07:02 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Rota Sorocabana!</h1>
+      <p>Esse é um site para bares, restaurantes e músicos.</p>
+    </div>
+  );
+}

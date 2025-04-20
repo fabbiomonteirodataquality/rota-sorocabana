ce07bbf2307a121b763fb985a039433ced0955fe
fabbiomonteirodataquality <50607094+fabbiomonteirodataquality@users.noreply.github.com>
2025-04-20 16:29:14 -0300

    Create bar.js

---
+++ pages/bar.js
@@ -0,0 +1,8 @@
+export default function BarPage() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-4">Cadastro de Bares</h1>
+      <p>Aqui os donos de bares poderão cadastrar seus estabelecimentos. (Aprovação manual requerida)</p>
+    </div>
+  );
+}

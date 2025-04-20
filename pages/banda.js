ea0f26b48fb8e137ef853a72a6bdbc53a85ceb67
fabbiomonteirodataquality <50607094+fabbiomonteirodataquality@users.noreply.github.com>
2025-04-20 16:29:32 -0300

    Create banda.js

---
+++ pages/banda.js
@@ -0,0 +1,8 @@
+export default function BandaPage() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-4">Cadastro de Músicos</h1>
+      <p>Aqui músicos e bandas poderão divulgar seus perfis por estilo musical.</p>
+    </div>
+  );
+}
